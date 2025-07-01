0c5c0707524b482e6b481c63807e6c094ffe6ae9
Vinícios Lacerda <lacerdevy@gmail.com>
2025-07-01 16:11:19 -0300

    Feat: Criando a estrutura html e css do app para cada seção do site

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,32 @@
 import "./styles/theme.css";
 import "./styles/global.css";
 
-import { Heading } from "./components/Heading";
-import { TimerIcon } from "lucide-react";
-
 export function App() {
   return (
-    <div>
-      <Heading>
-        Olá Mundo
-        <button>
-          <TimerIcon />
-        </button>
-      </Heading>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vero numquam
-        culpa fugit magni assumenda repellendus a officia deserunt? Doloribus
-        nam cum blanditiis facilis tempora dolorem voluptas vitae, iste ut
-        explicabo.
-      </p>
-    </div>
+    <>
+      <div className="container">
+        <div className="content">
+          <section>LOGO</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>MENU</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>FORM</section>
+        </div>
+      </div>
+
+      <div className="container">
+        <div className="content">
+          <section>FOOTER</section>
+        </div>
+      </div>
+    </>
   );
 }
