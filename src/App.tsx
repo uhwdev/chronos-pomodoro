83296fd740094989bfcf34345ac0536479f17906
Vinícios Lacerda <lacerdevy@gmail.com>
2025-07-07 18:14:56 -0300

    Chore: Limpando o código

--- src/App.tsx
+++ src/App.tsx
@@ -6,34 +6,13 @@ import { DefaultInput } from "./components/DefaultInput";
 import { Cycles } from "./components/Cycles";
 import { DefaultButton } from "./components/DefaultButton";
 import { Footer } from "./components/Footer";
-import { Heading } from "./components/Heading";
-
 import "./styles/theme.css";
 import "./styles/global.css";
 import { PlayCircleIcon } from "lucide-react";
-import { useState } from "react";
 
 export function App() {
-  // Quero que todos os componenetes que usam "numero"
-  // Saibam das mudanças em seu valor
-
-  // Sempre que eu usar o useStates, não vou usar atribuição diretamente "numero += 1"  //Se o Programa for pesado para inicializar utilizar dessa forma o useState
-  // const [numero, setNumero] = useState(() => {
-  //   console.log("Lazy initialization");
-  //   return 0;
-  // });
-
-  const [numero, setNumero] = useState(0);
-
-  function handleClick() {
-    // setNumero((prevState) => prevState + 1); Se for usar o valor anterior, utilzizar assim
-    setNumero(1);
-  }
-
   return (
     <>
-      <Heading> Número: {numero}</Heading>
-      <button onClick={handleClick}>Aumenta</button>
       <Container>
         <Logo />
       </Container>
@@ -50,7 +29,7 @@ export function App() {
         <form className="form" action="">
           <div className="formRow">
             <DefaultInput
-              labelText={numero.toString()}
+              labelText="task"
               id="meuInput"
               type="text"
               placeholder="Digite Algo"
