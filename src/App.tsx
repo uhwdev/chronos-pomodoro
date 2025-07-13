eb4e04b60a5d6f8130b88244ffaa5b4d60ffe3b5
Vinícios Lacerda <lacerdevy@gmail.com>
2025-07-13 11:59:16 -0300

    docs: Organizando o contexto em arquivos separados

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import { Home } from "./pages/Home";
-import { TaskContextProvider } from "./contexts/TaskContext";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { TaskContextProvider } from "./contexts/TaskContext/TaskContextProvider";
 
 export function App() {
   return (
