b5fa89f4b64bd02ba390e83ecf89373e99457b76
Vinícios Lacerda <lacerdevy@gmail.com>
2025-07-13 10:52:54 -0300

    feat: Estado global da aplicação com useState e comunicação via props

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,24 @@ import { Home } from "./pages/Home";
 
 import "./styles/theme.css";
 import "./styles/global.css";
+import { useState } from "react";
+import type { TaskStateModel } from "./models/TaskStateModel";
+
+const initialState: TaskStateModel = {
+  tasks: [],
+  secondsRemaining: 0,
+  formattedSecondsRemaining: "00:00",
+  activetask: null,
+  currentCycle: 0,
+  config: {
+    workTime: 25,
+    shortBreakTime: 5,
+    longBreakTime: 15,
+  },
+};
 
 export function App() {
-  return <Home />;
+  const [state, setState] = useState(initialState);
+
+  return <Home state={state} setState={setState} />;
 }
--- src/models/TaskStateModel.ts
+++ src/models/TaskStateModel.ts
@@ -1,14 +1,16 @@
 import type { TaskModel } from "./TaskModel";
 
+// Estado -> Componente -> Filhos
+
 export type TaskStateModel = {
-  tasks: TaskModel[];
-  secondsRemaining: number;
-  formattedSecondsRemaining: string;
-  activetask: TaskModel | null;
+  tasks: TaskModel[]; //Histórico, MainForm
+  secondsRemaining: number; // Home, CountDown, Histórico, MainForm, Button
+  formattedSecondsRemaining: string; // Título, CountDown
+  activetask: TaskModel | null; // CountDown, Histórico, MainForm, Button
   currentCycle: number; //1 a 8
   config: {
-    workTime: number;
-    shortBreakTime: number;
-    longBreakTime: number;
+    workTime: number; // MainForm
+    shortBreakTime: number; // MainForm
+    longBreakTime: number; // MainForm
   };
 };
