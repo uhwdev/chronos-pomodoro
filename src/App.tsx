240ca96ba657418a66140b4e75f06e02dae5b9ba
Vinícios Lacerda <lacerdevy@gmail.com>
2025-07-13 11:18:04 -0300

    feat: Context API - criando um context com createContext, useContext + Provider

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import "./styles/theme.css";
 import "./styles/global.css";
 import { useState } from "react";
 import type { TaskStateModel } from "./models/TaskStateModel";
+import { TaskContext } from "./contexts/TaskContext";
 
 const initialState: TaskStateModel = {
   tasks: [],
@@ -21,5 +22,9 @@ const initialState: TaskStateModel = {
 export function App() {
   const [state, setState] = useState(initialState);
 
-  return <Home />;
+  return (
+    <TaskContext.Provider value={{ outraCoisa: 321 }}>
+      <Home />;
+    </TaskContext.Provider>
+  );
 }
